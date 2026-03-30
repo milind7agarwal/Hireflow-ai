08e763e53787e88fe80efc8151e44946dcc421c7
Milind Agarwal <agarwalmilind77@gmail.com>
2026-03-31 00:06:24 +0530

    fix:// backend fix

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,7 +6,7 @@ const cors = require("cors")
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ["https://hireflow-ai-ashy.vercel.app/login","*"],
     credentials: true
 }))
 
