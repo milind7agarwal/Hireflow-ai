5479ea56bd7de7821b86e7165748705cb5cce7fd
Milind Agarwal <agarwalmilind77@gmail.com>
2026-03-31 00:17:05 +0530

    fix:// route fix in fe

--- frontend/src/features/auth/services/auth.api.js
+++ frontend/src/features/auth/services/auth.api.js
@@ -55,7 +55,7 @@ export async function getMe() {
 
     try {
 
-        const response = await api.get("/api/auth/get-me")
+        const response = await api.get("/api/auth/me")
 
         return response.data
 
