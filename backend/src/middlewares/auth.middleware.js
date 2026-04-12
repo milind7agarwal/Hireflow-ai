d04523822c97ec2fe84fac2aa9f71461c3fe245b
Mind07 <155805902+milind7agarwal@users.noreply.github.com>
2026-04-12 13:44:04 +0530

    Refactor auth middleware for token validation

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -1,37 +1,43 @@
 const jwt = require("jsonwebtoken")
-const tokenBlacklistedModel = require("../models/blacklist.model.js")
+const tokenBlacklistModel = require("../models/blacklist.model")
 
-async function authUser(req,res,next){
+
+
+async function authUser(req, res, next) {
 
     const token = req.cookies.token
 
-    if(!token){
+    if (!token) {
         return res.status(401).json({
-            message:"token not provided"
+            message: "Token not provided."
         })
     }
 
-    const isTokenBlackisted = await tokenBlacklistedModel
-        .findOne({ token })
-        .lean()
+    const isTokenBlacklisted = await tokenBlacklistModel.findOne({
+        token
+    })
 
-    if(isTokenBlackisted) {
+    if (isTokenBlacklisted) {
         return res.status(401).json({
-            messgae: "token is invalid"
+            message: "token is invalid"
         })
     }
 
-    try{
+    try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
         req.user = decoded
 
         next()
-    }catch(err){
+
+    } catch (err) {
+
         return res.status(401).json({
-            message: "Invalid Token"
+            message: "Invalid token."
         })
     }
+
 }
+
 
 module.exports = { authUser }
