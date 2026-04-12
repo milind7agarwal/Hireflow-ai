834fd35fdf39c767f602d4a15f1bd0120c68f2e9
Mind07 <155805902+milind7agarwal@users.noreply.github.com>
2026-04-12 13:43:23 +0530

    Remove unused tokenBlacklistModel import

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,6 +1,4 @@
 const mongoose = require("mongoose")
-const tokenBlacklistModel = require("../models/blacklist.model")
-
 async function connectToDB(){
     try{
     await mongoose.connect(process.env.MONGO_URL)
