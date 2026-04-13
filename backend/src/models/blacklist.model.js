6d706643688100f5a8bd14f4dfaacdeb7d2b15f2
Mind07 <155805902+milind7agarwal@users.noreply.github.com>
2026-04-13 08:53:57 +0530

    Fix formatting and whitespace in blacklist model

--- backend/src/models/blacklist.model.js
+++ backend/src/models/blacklist.model.js
@@ -1,18 +1,16 @@
-const mongoose = require("mongoose")
+const mongoose = require('mongoose')
 
 
 const blacklistTokenSchema = new mongoose.Schema({
-    token:{
+    token: {
         type: String,
-        required: [true, "token is required to be added in blacklist"]
+        required: [ true, "token is required to be added in blacklist" ]
     }
-},{
-        timestamps: true
+}, {
+    timestamps: true
 })
 
-// Ensure fast blacklist checks by token string.
-blacklistTokenSchema.index({ token: 1 })
-
 const tokenBlacklistModel = mongoose.model("blacklistTokens", blacklistTokenSchema)
+
 
 module.exports = tokenBlacklistModel
