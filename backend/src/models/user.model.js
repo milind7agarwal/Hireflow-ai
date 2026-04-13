2c8c2866b5359d0319aecac47aae19387a9c0c1e
Mind07 <155805902+milind7agarwal@users.noreply.github.com>
2026-04-13 08:55:11 +0530

    Update user model schema and error messages

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,24 +1,25 @@
 const mongoose = require("mongoose")
 
+
 const userSchema = new mongoose.Schema({
-    username:{
+    username: {
         type: String,
-        unique:[true,"username is not available"],
-        required: true
+        unique: [ true, "username already taken" ],
+        required: true,
     },
 
-    email:{
+    email: {
         type: String,
-        unique: [true, "account already exist"],
-        required: true
+        unique: [ true, "Account already exists with this email address" ],
+        required: true,
     },
 
-    password:{
+    password: {
         type: String,
         required: true
     }
 })
 
-const userModel = mongoose.model("user",userSchema)
+const userModel = mongoose.model("users", userSchema)
 
 module.exports = userModel
