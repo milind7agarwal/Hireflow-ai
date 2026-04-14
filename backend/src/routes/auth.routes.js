860105ccd2bc5737296efb1143ffaafce0ace87f
Mind07 <155805902+milind7agarwal@users.noreply.github.com>
2026-04-14 09:35:30 +0530

    Update route descriptions and endpoint names in auth.routes.js

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -5,33 +5,35 @@ const authMiddleware = require("../middlewares/auth.middleware")
 const authRouter = Router()
 
 /**
- *  @route POST /api/auth/register
- *  @description Register a new user
- *  @access Public
+ * @route POST /api/auth/register
+ * @description Register a new user
+ * @access Public
  */
-
 authRouter.post("/register", authController.registerUserController)
 
+
 /**
- *  @route POST /api/auth/login
- *  @description login a user
- *  @access Public
+ * @route POST /api/auth/login
+ * @description login user with email and password
+ * @access Public
  */
 authRouter.post("/login", authController.loginUserController)
 
+
 /**
- *  @route GET /api/auth/logout
- *  @description clear token from user cookie and add user in the blacklist
- *  @access Public
+ * @route GET /api/auth/logout
+ * @description clear token from user cookie and add the token in blacklist
+ * @access public
  */
 authRouter.get("/logout", authController.logoutUserController)
 
+
 /**
- *  @route GET /api/auth/get-me
- *  @description get the current logged in user details
- *  @access private
+ * @route GET /api/auth/get-me
+ * @description get the current logged in user details
+ * @access private
  */
-authRouter.get("/me",authMiddleware.authUser, authController.getMeController)
+authRouter.get("/get-me", authMiddleware.authUser, authController.getMeController)
 
 
 module.exports = authRouter
