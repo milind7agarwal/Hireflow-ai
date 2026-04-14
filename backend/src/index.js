29bd16baa3a588f386144eaabb3d2e5fb3c474e2
Mind07 <155805902+milind7agarwal@users.noreply.github.com>
2026-04-14 09:38:29 +0530

    Refactor index.js for code consistency and clarity

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,21 +1,25 @@
-const express = require('express')
-const app = express()
+const express = require("express")
 const cookieParser = require("cookie-parser")
 const cors = require("cors")
+
+const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: ["https://hireflow-ai-ashy.vercel.app","*"],
+    origin: "http://localhost:5173",
     credentials: true
 }))
 
-// require all the routes here
+/* require all the routes here */
 const authRouter = require("./routes/auth.routes")
 const interviewRouter = require("./routes/interview.routes")
 
-// using the routes
+
+/* using all the routes here */
 app.use("/api/auth", authRouter)
 app.use("/api/interview", interviewRouter)
 
-module.exports = app 
+
+
+module.exports = app
